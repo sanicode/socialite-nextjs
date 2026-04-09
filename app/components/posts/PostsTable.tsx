2de793bb0e56dd3283d6cf798c7487d56f7b51e3
Sani Iman <sani@staf.unair.ac.id>
2026-04-09 21:45:07 +0700

    Hide Action Column in role manager

--- app/components/posts/PostsTable.tsx
+++ app/components/posts/PostsTable.tsx
@@ -376,7 +376,7 @@ export default function PostsTable({ posts, total, categories, page, isAdmin, ca
                   )}
 
                   {/* Actions */}
-                  {(isAdmin || canVerify) && (
+                  {(isAdmin) && (
                   <td className="px-4 py-3">
                     <div className="flex items-center justify-end gap-2">
                       {canVerify && !isAdmin ? (
