444d34e1870c3cde0ef2ffbb40b9104b8a689aaa
Sani Iman Pribadi <sani.iman.pribadi@gmail.com>
2026-05-13 21:30:21 +0700

    revert: restore Next proxy file

--- proxy.ts
+++ proxy.ts
@@ -1,7 +1,7 @@
 import type { NextRequest } from 'next/server'
 import { NextResponse } from 'next/server'
 
-export function middleware(request: NextRequest) {
+export function proxy(request: NextRequest) {
   const requestHeaders = new Headers(request.headers)
   requestHeaders.set('x-request-path', request.nextUrl.pathname)
   requestHeaders.set('x-request-method', request.method)
